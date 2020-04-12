19647d927644592f62b15cb0012c1003307c9d96
ComeTiss <cometisserand@rocketmail.com>
2020-04-12 11:31:15 +0200

    [DB] Typescript compile allowjs + fix migration cmd

---
+++ src/db/config/config.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const DIALECT = "postgres";
+const DEV_HOST = "localhost";
+const DEV_USERNAME = "user1";
+const DEV_PASSWORD = "pass";
+
+module.exports = {
+  development: {
+    username: DEV_USERNAME,
+    password: DEV_PASSWORD,
+    database: "medline_dev",
+    host: DEV_HOST,
+    dialect: DIALECT,
+  },
+  test: {
+    username: DEV_USERNAME,
+    password: DEV_PASSWORD,
+    database: "medline_test",
+    host: DEV_HOST,
+    dialect: DIALECT,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: DIALECT,
+  },
+};
