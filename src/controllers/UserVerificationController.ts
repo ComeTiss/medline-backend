55a88bd66e915667b08f6bd21573ccfaea261b8b
ComeTiss <cometisserand@rocketmail.com>
2020-04-16 11:02:32 +0200

    [API] added new enpoint to send email confirmation

--- src/controllers/UserVerificationController.ts
+++ src/controllers/UserVerificationController.ts
@@ -1,12 +1,53 @@
 
-import { decodeJwt } from "../utils/auth/jwtUtils";
+import { decodeJwt, forgeJwt } from "../utils/auth/jwtUtils";
 import UserDao from "../dao/UserDao";
+import EmailUtils from "../utils/email/emailUtils";
 
 const URL_DEV = "http://localhost:3001"; // or 3000
 const URL_PROD = "https://medline-frontend.herokuapp.com";
 const REDIRECT_URL = process.env.NODE_ENV === "production" ? URL_PROD : URL_DEV;
 
 export default {
+  async sendEmailConfirmation(req, res) {
+    try {
+      if (!req?.body || !req?.body?.email) {
+        return res.status(403).send({
+          error: "Forbidden request",
+        });
+      }
+      const { email } = req.body;
+      const user = await UserDao.findOneByEmail(email);
+      if (!user) {
+        return res.status(401).send({
+          error: "Invalid email adress",
+        });
+      }
+      if (user.verifiedAt) {
+        return res.status(401).send({
+          error: "User already verified",
+        });
+      }
+
+      const token = await forgeJwt(user);
+      EmailUtils.sendMailConfirmation({
+        destinator: {
+          name: email,
+          email,
+        },
+        token,
+      });
+
+      return res.status(200).send({
+        message: "Successfull email request",
+        email,
+      });
+    } catch {
+      return res.status(500).send({
+        error: "Internal error occured",
+      });
+    }
+  },
+
   async verifiyUser(req, res) {
     try {
       if (!req?.params?.token) {
