678635ab9738194b6d7cc35389ff36c110cee983
Lucas Zimmermann <lucaszimmermann@Lucass-MacBook-Pro.local>
2020-04-30 12:29:36 +0200

    [Organization] Add mutation

--- src/graphql/resolvers/OrganizationResolvers.ts
+++ src/graphql/resolvers/OrganizationResolvers.ts
@@ -4,9 +4,23 @@ async function getOrganizationsWithOptions(root, args) {
   return OrganizationDao.getWithOptions(args.request);
 }
 
+async function mutateOrganization(obj, args, context) {
+  const { organizationId } = context;
+  const { request } = args;
+  console.log(organizationId);
+  console.log(request.id);
+
+  if (organizationId !== Number(request.id)) throw new Error("Organization does not match user profile");
+
+  return OrganizationDao.update(request);
+}
+
 const resolvers = {
   Query: {
     getOrganizationsWithOptions,
+  },
+  Mutation: {
+    mutateOrganization,
   },
 };
 
